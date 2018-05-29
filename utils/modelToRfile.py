408496ea8487b54e0e3ba634be53489c70f0f41f
firstkingofrome <username@users.noreply.github.com>
2018-05-29 12:51:00 -0700

    the reasont that I was having issues with the itnerpolater is because it expected lists of all COORDINATES, not evenly spaced BASIS (dugh)

--- utils/modelToRfile.py
+++ utils/modelToRfile.py
@@ -4,6 +4,12 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+#from scipy.spatial import KDTree
+#from scipy.ndimage import *
+#from scipy.interpolate.griddata import *
+import scipy
+import scipy.ndimage
+from scipy.interpolate import griddata
 
 #if this is false negetive numbers count as below sea level (the oppsite of how they are in SW4 rFiles)
 seaLevel = False
@@ -44,12 +50,9 @@ class Block():
         self.xyextent = self.y_extent + self.x_extent
         self.xzextent = self.x_extent + self.z_extent
         self.yzextent = self.y_extent + self.z_extent
-        
-
-        
-    def project(self):
-        #for use on topography data block 1 only (basically cuts off everything below the surface)
         pass
+        
+                
 
 class Parameterfile():
     def __init__(self,pFname = "rFile.ini"):
@@ -72,16 +75,16 @@ class Model():
         self.blocks = [Block(self.Parameterfile.pfContents["BLOCK_CONTROL"][i]) for i in self.Parameterfile.pfContents["BLOCK_CONTROL"].keys() if (i!="HEADER")]
         #load the information from the model descritption
         self.loadModel(fname)
-        #find the datums
-        self.minX = min(self.ModelFileData[:,0])        
-        self.minY = min(self.ModelFileData[:,1])
                
     #clean this up to make it faster at a later point
     def loadModel(self,fname):
         lines = []
         self.header = []
         self.ModelFileData = []
+        numLines = 0
+        dataStart = 0
         with open( fname,'r') as fileObject:
+
             while True:
                 #get the header
                 line =  fileObject.readline()
@@ -89,136 +92,124 @@ class Model():
                 self.header.append(line)
                 if("#" not in line):
                     break                    
-                    
+                   
             for line in fileObject:           
                 #assign everything to the nearest point
                 #the multiplication by 1000 moves from m/s to km/s
                 lines = [i+" " for i in line.split() if i != ""]  
                 #fix everything according to the datums           
                 #also internally I assign 0 to be the bottome of the rfile!b             
-                x = int(float(lines[0])-self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["xDatum"])  
-                y = int(float(lines[1])-self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["yDatum"])
-                z = int(float(lines[2]) + self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["maxDepthBelowSeaLvl"] if float(lines[2]) > 0 else abs(float(lines[2]) + self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["maxDepthBelowSeaLvl"]))
+                x = int((float(lines[0])-self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["xDatum"])/self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["baseResolution"])
+                y = int((float(lines[1])-self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["yDatum"])/self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["baseResolution"])
+                z = int(abs(float(lines[2]) - self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["maxDepthBelowSeaLvl"]) if float(lines[2]) > 0 else abs(float(lines[2])) + self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["maxDepthBelowSeaLvl"])/self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["baseResolution"]
                 
-                self.ModelFileData.append((x,y,z,int(lines[3])))
-                
-                
+                self.ModelFileData.append((x,y,z,int(lines[3])))                                
                 #self.ModelFileData.append((int(float(lines[0])-self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["xDatum"]),int(float(lines[1])-self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["yDatum"]),float(lines[2]),int(lines[3]) if int(lines[3])))
                 
         #now convert it to a numpy array for easier use
         self.ModelFileData = np.array(self.ModelFileData,dtype=np.float32)
-        #correct the up down orientation
-        self.ModelFileData = np.flipud(self.ModelFileData)
 
+        #correct the up down orientation
+        #self.ModelFileData = np.flipud(self.ModelFileData)
+
+        #find datums
+        self.minX = min(self.ModelFileData[:,0])        
+        self.minY = min(self.ModelFileData[:,1])
+        #put this into 3d grid space        
+        x,y,z=np.mgrid[0:len(np.unique(self.ModelFileData[:,0])),0:len(np.unique(self.ModelFileData[:,1])) ,0:len(np.unique(self.ModelFileData[:,2]))]
+
+        #assign data to "nearest" point on the mesh
+        self.inputModel = griddata((self.ModelFileData[:,0].astype(int),self.ModelFileData[:,1].astype(int),self.ModelFileData[:,2]),(self.ModelFileData[:,3]).astype(int),(x,y,z),method='nearest')
         
-        
+        #free up memory by removing the origional data
+        #TODO uncomment this for the actualy production code!
+        #del self.ModelFileData                                
         
     #fix coordinates (i.e use cartesian coordinates)
     def fixCoordinates(self):
         #find the minimum value for all of the UTM coordinates and assign accordingly
-        
         pass
         
-    """
-    coordsToIndex
-    
-    Parameters
-    ----------
-    east: float
-        UTM easting coordinate for the bottom left most coordinate in rFile
-
-    north : float
-        UTM northing coordinate for the bottom left most coordinate in rFile     
-
-    depth : float
-        current depth in model (note that this follows the rFile depth convention, negetiv == above sea level depth
-        
-
-    depthDelta : float
-        Depth delta (i.e 10 means that every pixel is sepperated by 10M depth)
-
-    horrizontalDelta: float
-        horrizontal delta (i.e 10 means that every pixel in xy space is 10M square)
-
-    Ni : int
-        #i coordinates for this block
-        
-    Ni : bool
-        #j coordinates for this block
-        
-    Nk: 
-        #k coordinates for this block
-        
-    """
-        
-    def coordsToIndex(self,depthDatum,depthDelta,horrizontalDelta,Ni,Nj,Nk,xDatum,yDatum,depthVal,xVal,yVal):        
-        #datums which are always the same since each block will always have the same datum  (i.e rfiles describe 
-        #return the index (this should map everything to the nearest pixel? test IT EMPIRICALLY!!
-        return int(((xVal-xDatum)/(Ni*horrizontalDelta)) + ((yVal-yDatum)/(Nj*horrizontalDelta))
-        + ((depthVal-abs(depthDatum))/(Nk*depthDelta)))
-            
-    def buildRfile(self,fileObject): 
-        blockExtent = []     
+    def buildRfile(self,fileObject):   
+        i =0
+        j=0
+        k=0
+        x=0
+        y=0
+        z=0
+        componentMesh = []
         blockIndex = 1  
-        dataArrayDimensions = []
-        xCoord = 0
-        yCoord = 0
-        zCoord = 0
+        baseDepth = 0
+        lastBlockBaseDepth = 0
+
         #I am just going to do these all in one go to start with and I will deal with memory issues later        
         #construct topography block, assign to 0 for now, then once I have the data assign to the data
         #TODO assign this to topography correctly
         self.blocks[0].topo = np.full((self.blocks[0].ni,self.blocks[0].nj),0,dtype=np.float32)
         self.blocks[0].topo[:] = 0        
-
         #save datums from header and remove all other uneeded crap
-   
+      
+        #compute material properties before saving block data
+        
         #save the block data
         for block in self.Parameterfile.pfContents["BLOCK_CONTROL"]:
             #make sure that I am dealing with an actualy BLOCK
-            if(block != "TOPO" and block != "HEADER"):                               
-                #subset the input data by the current block (basically get a list of the indexes that correspond to data that is within the current block
-                #takes everything within (inclusive) depth range for this block)
-                blockExtent = np.where(np.logical_and(np.less_equal(self.ModelFileData[:,2],self.Parameterfile.pfContents["BLOCK_CONTROL"][block]["Z0"]),np.greater_equal(self.ModelFileData[:,2],self.Parameterfile.pfContents["BLOCK_CONTROL"][block]["Z0"] 
-                - (self.Parameterfile.pfContents["BLOCK_CONTROL"][block]["Nk"]*self.Parameterfile.pfContents["BLOCK_CONTROL"][block]["HVb"]))))[0]
+            if(block != "TOPO" and block != "HEADER"):    
+                print("Iteriation ",block)                           
+                #construct component mesh for points in this space
+                meshGrid = np.mgrid[0:(self.blocks[blockIndex].ni*self.blocks[blockIndex].hh)/self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["baseResolution"]
+                ,0:self.blocks[blockIndex].nj*self.blocks[blockIndex].hh/self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["baseResolution"]
+                ,0:self.blocks[blockIndex].nk * self.blocks[blockIndex].hv/self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["baseResolution"]]                
                 
-                #compute the data array dimensions (note these may exceed i,j and k)
-                dataArrayDimensions = [self.blocks[blockIndex].ni,self.blocks[blockIndex].nj,self.blocks[blockIndex].nk]
-                self.blocks[blockIndex].vp = np.nan*np.empty(dataArrayDimensions)
-                #now assign to this based on the availibility of points in the underlying model
-                for i in range(len(self.ModelFileData[blockExtent][:,0])):    
+                #compute the depth datum--using my modified coordinates
+                lastBlockBaseDepth = baseDepth
+                baseDepth = (abs(self.Parameterfile.pfContents["BLOCK_CONTROL"][block]["Z0"]) - self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["maxDepthBelowSeaLvl"]) if (self.Parameterfile.pfContents["BLOCK_CONTROL"][block]["Z0"]) > 0 else (abs(self.Parameterfile.pfContents["BLOCK_CONTROL"][block]["Z0"])-self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["maxDepthBelowSeaLvl"] 
+                /self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["baseResolution"])
+                #correct base depth for the underlying resolution
+                baseDepth = baseDepth/self.Parameterfile.pfContents["BLOCK_CONTROL"]["HEADER"]["baseResolution"]
+                #compute coordinate sets
+                x,y,z = np.meshgrid(np.arange(self.inputModel.shape[0]),np.arange(self.inputModel.shape[1]),np.arange(self.inputModel[:,:,lastBlockBaseDepth:baseDepth].shape[2]))           
+                
+                #x,y,z = self.inputModel[:,:,lastBlockBaseDepth:baseDepth].shape
+                #now assign to this based on the availibility of points in the underlying model                                
+                #test = scipy.interpolate.griddata((x,y,z,([self.getVP(i) for i in self.inputModel[:,:,lastBlockBaseDepth:baseDepth].flatten()]),(meshGrid[0],meshGrid[1],meshGrid[2]),method='nearest'))
+                X = meshGrid[0]
+                Y = meshGrid[1]
+                Z = meshGrid[2]
+                #vp
+                
+                
+                blockData = np.asarray([self.getVP(i) for i in np.nditer(self.inputModel[:,:,lastBlockBaseDepth:baseDepth])])
+                #the flattening is because what I essentially have in grid data is a basis and I want EVERY coordinate pair!
+                test = griddata((x.flatten(),y.flatten(),z.flatten()),blockData,(meshGrid[0],meshGrid[1],meshGrid[2]),method='nearest')
+                                
+                #vs
+                
+                
+                #p
+                
+                                
+                #qp
+                
+                                
+                #qs
+                
+                for i in self.inputModel:                    
                     #take the coordinate, scale according to ni and hh and assign
-                    xCoord = int(self.ModelFileData[blockExtent][i][0]/(self.blocks[blockIndex].ni*self.blocks[blockIndex].hh))
-                    yCoord = int(self.ModelFileData[blockExtent][i][1]/(self.blocks[blockIndex].nj*self.blocks[blockIndex].hh))
-                    zCoord = int(self.ModelFileData[blockExtent][i][2]/(self.blocks[blockIndex].nk*self.blocks[blockIndex].hv))                    
-                    self.blocks[blockIndex].vp[xCoord][yCoord][zCoord] = self.getVP(self.ModelFileData[blockExtent][i][3])                                        
-                    #do this for all components!
                     
-                    #done
-                print("finished Block!")
-                print(xCoord)
-                print(yCoord)
-                print(zCoord)
-                print(self.blocks[blockIndex].vp)
-                print(block)
-                #assign based on the number of components availible                                         
-                                
-                #linearly interpolate this block
-                                                
-                #save this block
-                                
-                #(optional) return the memory used by this block
+                    pass
                 
                 #next block 
                 blockIndex += 1
                 pass
-            
-
+        
         #halt
         #free memory held by base model 
         #now linearly interpolate to fill all of the blocks
         #write the rFile
         print("finished!")
         pass
+    
     
     #get all of the charecteristics
     #TODO add support to compute this from model specified in parameter file, that is WHY THIS IS A FUNCTION INSTEAD OF JUST AND ASSIGNMENT
@@ -239,26 +230,10 @@ class Model():
     def getP(self,unit):
         return self.Parameterfile.pfContents["GEOL_CONTROL"][str(unit).split(".")[0]]["P"]
 
-
 def main():
-    #allocate pixel space
-    
-    #load the geologic model and map each point to pixel space (at the same time)
-    
-    #for each block
-    
-        #interpolate
-        
-        #save pixel space to the rFile
-        
-        #reallocate memory 
-        
-    #done
-    
+    # find 10 nearest points
+    tree = KDTree(a, leafsize=a.shape[0]+1)
+    distances, ndx = tree.query([point], k=10)
+    # print 10 nearest points to the chosen one
+    print a[ndx]
     pass
-
-if __name__=="__main__":
-    main()
-    pass
-
-
